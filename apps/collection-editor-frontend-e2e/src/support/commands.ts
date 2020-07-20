41083b076056162e8116b51739a987580acf8d1a
Mateusz Gorczycki <mateusz.gorczycki@whiteaster.com>
2020-07-20 11:28:39 +0200

    Add datatables and change commands.ts

--- apps/collection-editor-frontend-e2e/src/support/commands.ts
+++ apps/collection-editor-frontend-e2e/src/support/commands.ts
@@ -8,6 +8,7 @@
 // https://on.cypress.io/custom-commands
 // ***********************************************
 // eslint-disable-next-line @typescript-eslint/no-namespace
+/* tslint:disable */
 declare namespace Cypress {
   interface Chainable<Subject> {
     login(email: string, password: string): void;
