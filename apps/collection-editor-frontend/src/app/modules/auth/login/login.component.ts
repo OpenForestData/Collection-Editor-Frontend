6e2a205174cdf707230bf577b9da932e39b843fd
Mateusz Gorczycki <mateusz.gorczycki@whiteaster.com>
2020-08-03 10:00:20 +0200

    Fix redirect to login page

--- apps/collection-editor-frontend/src/app/modules/auth/login/login.component.ts
+++ apps/collection-editor-frontend/src/app/modules/auth/login/login.component.ts
@@ -57,7 +57,7 @@ export class LoginComponent implements OnInit {
       password: ['', Validators.required],
     });
 
-    this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+    this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/auth/login';
   }
 
   /**
