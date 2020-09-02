f4a64381537c811e091b537c59bc729b38958f29
Mateusz Gorczycki <mateusz.gorczycki@whiteaster.com>
2020-09-02 15:09:35 +0200

    Add clipboard and export to pdf, csv

--- apps/collection-editor-frontend/src/app/shared/shared.module.ts
+++ apps/collection-editor-frontend/src/app/shared/shared.module.ts
@@ -3,10 +3,20 @@ import { CommonModule } from '@angular/common';
 import { MaterialModule } from './material.module';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { ConfirmationDialogComponent } from './confirmation-dialog/confirmation-dialog.component';
+import { MatTableExporterModule } from 'mat-table-exporter';
+import { ClipboardModule } from 'ngx-clipboard';
 
 @NgModule({
-  imports: [CommonModule, MaterialModule, ReactiveFormsModule, FormsModule],
+  imports: [CommonModule, MaterialModule, ReactiveFormsModule, FormsModule, MatTableExporterModule, ClipboardModule],
   declarations: [ConfirmationDialogComponent],
-  exports: [CommonModule, MaterialModule, ReactiveFormsModule, FormsModule, ConfirmationDialogComponent],
+  exports: [
+    CommonModule,
+    MaterialModule,
+    ReactiveFormsModule,
+    FormsModule,
+    ConfirmationDialogComponent,
+    MatTableExporterModule,
+    ClipboardModule,
+  ],
 })
 export class SharedModule {}
